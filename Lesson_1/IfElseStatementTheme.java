3184d0ceea95ea71ebf26fd2bccf51e52ada9b56
Maksimelgsk <konovalo_ms@mail.ru>
2022-10-15 12:04:02 +0300

    HW2_edit_15-10-2022

--- Lesson_1/IfElseStatementTheme.java
+++ Lesson_1/IfElseStatementTheme.java
@@ -1,6 +1,6 @@
 public class IfElseStatementTheme {
     public static void main(String[] args) {
-        System.out.println ("\nЗадача №1. Перевод псевдокода на язык Java" + "\n");
+        System.out.println ("\nЗадача №1. Перевод псевдокода на язык Java\n");
         int personAge = 21;
         if (personAge > 20) {
             System.out.println ("Возраст " + personAge + " - кредит одобрен");
@@ -28,7 +28,7 @@ public class IfElseStatementTheme {
             System.out.println("Первая буква имени " + firstLetterName + " - команда №3 ");
         }
 
-        System.out.println("\nЗадача №2. Поиск max и min числа" + "\n");
+        System.out.println("\nЗадача №2. Поиск max и min числа\n");
         int num1 = 2; 
         int num2 = 1;
         if (num1 > num2) {
@@ -39,7 +39,7 @@ public class IfElseStatementTheme {
             System.out.println("Числа равны " + num1 + " = " + num2);
         }
 
-        System.out.println("\nЗадача №3. Работа с числом" + "\n");
+        System.out.println("\nЗадача №3. Работа с числом\n");
         int num3 = 5;
         if (num3 == 0) {
             System.out.println("Число равно 0");
@@ -56,7 +56,7 @@ public class IfElseStatementTheme {
             }
         }
 
-        System.out.println("\nЗадача №4. Поиск одинаковых цифр в числах" + "\n");
+        System.out.println("\nЗадача №4. Поиск одинаковых цифр в числах\n");
         int num4 = 456;
         int num5 = 423;
         int hundredNum4 = (num4 / 100) % 10;
@@ -85,23 +85,23 @@ public class IfElseStatementTheme {
             System.out.println("Нет одинаковых цифр в 1-м разряде");
         }
 
-        System.out.println("\nЗадача №5. Определение буквы, числа или символа по их коду" + "\n");
+        System.out.println("\nЗадача №5. Определение буквы, числа или символа по их коду\n");
         char someSymbol = '\u0057';
         if(someSymbol >= 'A' && someSymbol <= 'Z') {
             System.out.println("Символ Char - " + "'" + someSymbol + "'" + ", его код Dec = " + 
-                    (int)'W' + " и это большая буква"); 
+                    (int) someSymbol + " и это большая буква"); 
         } else if (someSymbol >= 'a' && someSymbol <= 'z') {
             System.out.println("Символ Char - " + "'" + someSymbol + "'" + ", его код Dec = " + 
-                    (int)someSymbol + " и это маленькая буква"); 
+                    (int) someSymbol + " и это маленькая буква"); 
         } else if (someSymbol >= '0' && someSymbol <= '9') {
             System.out.println("Символ Char - " + "'" + someSymbol + "'" + ", его код Dec = " + 
-                    (int)someSymbol + " и это число");
+                    (int) someSymbol + " и это число");
         } else {
             System.out.println("Символ Char - " + "'" + someSymbol + "'" + ", его код Dec = " + 
-                    (int)someSymbol + " и это не буква и не число, а символ");
+                    (int) someSymbol + " и это не буква и не число, а символ");
         }
 
-        System.out.println("\nЗадача №6. Определение суммы вклада и начисленных банком %" + "\n");
+        System.out.println("\nЗадача №6. Определение суммы вклада и начисленных банком %\n");
         int depositSum = 300_000;
         // начисленный процент
         float profit;
@@ -117,76 +117,40 @@ public class IfElseStatementTheme {
         }
         System.out.println("Начисленный процент - " + profit + ". Итоговая сумма - " + (profit + depositSum));
 
-        System.out.println("\nЗадача №7. Определение оценки по предметам" + "\n");
+        System.out.println("\nЗадача №7. Определение оценки по предметам\n");
         // итоговый балл по истории
         int percentHistory = 59;
         // итоговый балл по программированию
-        int percentProg = 99;
-        // средний % по предметам
-        float mediumValuePercent = (percentHistory + percentProg) / 2F;
-        // средняя оценка
-        float mediumMark = 0;
+        int percentProg = 65;
         // оценка по истории
         int markHistory = 0;
         // оценка по программированию
         int markProg = 0;
         if ((percentHistory <= 0) || (percentHistory > 100) || (percentProg <= 0) || (percentProg > 100)) {
             System.out.println("Неверно введены данные!!! Повторите попытку");
-        } else if ((percentHistory <= 60) && (percentProg <=60)) {
+        } if (percentHistory <= 60) {
             markHistory = 2;
-            markProg = 2;
-        } else if ((percentHistory > 60) && (percentHistory <= 73) && (percentProg > 60) && (percentProg <=73)) {
+        } else if ((percentHistory > 60) && (percentHistory <= 73)) {
             markHistory = 3;
-            markProg = 3;
-        } else if ((percentHistory > 73) && (percentHistory <= 91) && (percentProg > 73) && (percentProg <=91)) {
+        } else if ((percentHistory > 73) && (percentHistory <= 91)) {
             markHistory = 4;
-            markProg = 4;
-        } else if ((percentHistory > 91) && (percentProg > 91)) {
+        } else if (percentHistory > 91) {
             markHistory = 5;
-            markProg = 5;
-        } else if ((percentHistory <= 60) && (percentProg > 60) && (percentProg <=73)) {
-            markHistory = 2;
-            markProg = 3;
-        } else if ((percentHistory <= 60) && (percentProg > 73) && (percentProg <=91)) {
-            markHistory = 2;
-            markProg = 4;
-        } else if ((percentHistory <= 60) && (percentProg > 91)) {
-            markHistory = 2;
-            markProg = 5;
-        } else if ((percentHistory > 60) && (percentHistory <= 73) && (percentProg <= 60)) {
-            markHistory = 3;
+        } if (percentProg <= 60) {
             markProg = 2;
-        } else if ((percentHistory > 73) && (percentHistory <= 91) && (percentProg <= 60)) {
-            markHistory = 4;
-            markProg = 2;
-        } else if ((percentHistory > 91) && (percentProg <= 60)) {
-            markHistory = 5;
-            markProg = 2;
-        } else if ((percentHistory > 60) && (percentHistory <= 73) && (percentProg >  73) && (percentProg <= 91)) {
-            markHistory = 3;
-            markProg = 4;
-        } else if ((percentHistory > 60) && (percentHistory <= 73) && (percentProg >  91)) {
-            markHistory = 3;
-            markProg = 5;
-        } else if ((percentHistory > 73) && (percentHistory <= 91) && (percentProg > 60) && (percentProg <=73)) {
-            markHistory = 4;
+        } else if ((percentProg > 60) && (percentProg <= 73)) {
             markProg = 3;
-        } else if ((percentHistory > 73) && (percentHistory <= 91) && (percentProg >  91)) {
-            markHistory = 4;
-            markProg = 5;
-        } else if ((percentHistory > 91) && (percentProg >60) && (percentProg <= 73)) {
-            markHistory = 5;
-            markProg = 3;
-        } else if ((percentHistory > 91) && (percentProg >73) && (percentProg <= 91)) {
-            markHistory = 5;
+        } else if ((percentProg > 73) && (percentProg <= 91)) {
             markProg = 4;
+        } else if (percentProg > 91) {
+            markProg = 5;
         }
         System.out.println("Оценка - " + markHistory + " история");
         System.out.println("Оценка - " + markProg + " программирование");
         System.out.println("Средний балл оценок по предметам - " + ((markHistory + markProg) / 2F));
-        System.out.println("Средний % по предметам - " + mediumValuePercent);
+        System.out.println("Средний % по предметам - " + ((percentHistory + percentProg) / 2F));
 
-        System.out.println("\nЗадача №8. Расчет прибыли" + "\n");
+        System.out.println("\nЗадача №8. Расчет прибыли\n");
         int rent = 5000;
         float monthlySales = 13000F;
         float costPrice = 9000F;
@@ -197,27 +161,26 @@ public class IfElseStatementTheme {
             System.out.println("Прибыль за год = " + annualProfit + " руб.");
         }
 
-        System.out.println("\nЗадача №9. Подсчет количества банкнот" + "\n");
-        int needSum = 567;
+        System.out.println("\nЗадача №9. Подсчет количества банкнот\n");
+        int needSum = 999;
         int usd100 = 100;
         int usd10 = 10;
         int usd1 = 1;
-        int sum100 = (needSum / 100) % 10;
-        int sum10 = (needSum / 10) % 10;
-        int sum1 = needSum % 10;
-        System.out.println("Номиналы банкнот в банкомате: " + usd100 + "$, " + usd10 + "$, " + usd1 + "$");
-        if ((needSum >= 100) && (needSum < 1000) && (sum10 >= 5)) {
-            int needSumUsd10 = 5;
-            System.out.println("Требуемое количество 100$ = " + sum100);
-            System.out.println("Требуемое количество 10$ = " + needSumUsd10);
-            System.out.println("Требуемое количество 1$ = " + (10 * sum10 - 10 * needSumUsd10 + sum1));
-            System.out.println("Исходная сумма = " + (sum100 * usd100 + usd10 * needSumUsd10 + usd1 * (10 * sum10 - 10 * needSumUsd10 + sum1)) + "$");
-        } else if ((needSum >= 100) && (needSum < 1000) && (sum10 < 5)) {
-            int needSumUsd_10 = 4;
-            System.out.println("Требуемое количество 100$ = " + sum100);
-            System.out.println("Требуемое количество 10$ = " + needSumUsd_10);
-            System.out.println("Требуемое количество 1$ = " + (10 * sum10 - 10 * needSumUsd_10 + sum1));
-            System.out.println("Исходная сумма = " + (sum100 * usd100 + usd10 * needSumUsd_10 + usd1 * (10 * sum10 - 10 * needSumUsd_10 + sum1)) + "$");
+        int amount100 = (needSum / 100) % 10;
+        int amount10 = (needSum / 10) % 10;
+        int amount1 = needSum % 10;
+        int needAmountUsd10 = 5;
+        System.out.println("Номиналы банкнот в банкомате: " + usd100 + "$ " + usd10 + "$ " + usd1 + "$");
+        if (needSum > 0 && needSum <= 1000) {
+            if (amount10 >= 5) {
+                needAmountUsd10 = 5;
+            } else if (amount10 < 5) {
+                needAmountUsd10 = amount10;
+            }
+        System.out.println("Требуемое количество 100$ = " + amount100);
+        System.out.println("Требуемое количество 10$ = " + needAmountUsd10);
+        System.out.println("Требуемое количество 1$ = " + (10 * amount10 - 10 * needAmountUsd10 + amount1));
+        System.out.println("Исходная сумма = " + (amount100 * usd100 + usd10 * needAmountUsd10 + usd1 * (10 * amount10 - 10 * needAmountUsd10 + amount1)) + "$");
         } else {
             System.out.println("В банкомате нет требуемой суммы!");
         }
