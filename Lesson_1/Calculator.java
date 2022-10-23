f1c13190915465c031a43508e4a15872859da5e9
Maksimelgsk <konovalo_ms@mail.ru>
2022-10-23 18:19:41 +0300

    Calculator_edit_23-10-2022

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,32 +1,30 @@
 public class Calculator {
     public static void main(String[] args) {
-        int a = 3;
-        int b = 2;
-        //выберите действие 1-"+"; 2-"-"; 3-"*"; 4-"/"; 5-"^"; 6-"%"
-        char sign = 4;
+        int a = 12;
+        int b = 10;
+        //выберите действие '+'; '-'; '*'; '/'; '^'; "%"
+        char sign = '^';
         int result = 0;
-        if (sign == 1 && a >= 0 && b >= 0) {
+        if (sign == '+') {
             result = a + b;
-            sign = '+';
-        } else if (sign == 2 && a >= 0 && b >= 0) {
+        } else if (sign == '-') {
             result = a - b;
-            sign = '-';
-        } else if (sign == 3 && a >= 0 && b >= 0) {
+        } else if (sign == '*') {
             result = a * b;
-            sign = '*';
-        } else if (sign == 4 && a >= 0 && b >= 0) {
+        } else if (sign == '/') {
             result = a / b;
-            sign = '/';
-        } else if (sign == 5 && a >= 0 && b >= 0) {
+        } else if (sign == '^') {
             result = 1;
             for (int i = 1; i <= b; i++) {
                 result = result * a;
             }
-            sign = '^';
-        } else if (sign == 6 && a >= 0 && b >= 0) {
+        } else if (sign == '%') {
             result = a % b;
-            sign = '%';
         }
-        System.out.println(a + " " + sign + " " + b + " = " + result);
+        if (a < 0 || b < 0) {
+            System.out.println("Введите целые положительные числа!!!");
+        } else {
+            System.out.println(a + " " + sign + " " + b + " = " + result);
+        }
     }
 }
