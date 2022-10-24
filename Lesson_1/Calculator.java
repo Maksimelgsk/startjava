f12448fe9b6bffe63f627161c0710e2d52b1d6fe
Maksimelgsk <konovalo_ms@mail.ru>
2022-10-24 21:13:15 +0300

    Calculator_edit_24-10-2022

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,29 +1,29 @@
 public class Calculator {
     public static void main(String[] args) {
-        int a = 12;
+        int a = 2;
         int b = 10;
         //выберите действие '+'; '-'; '*'; '/'; '^'; "%"
         char sign = '^';
         int result = 0;
-        if (sign == '+') {
-            result = a + b;
-        } else if (sign == '-') {
-            result = a - b;
-        } else if (sign == '*') {
-            result = a * b;
-        } else if (sign == '/') {
-            result = a / b;
-        } else if (sign == '^') {
-            result = 1;
-            for (int i = 1; i <= b; i++) {
-                result = result * a;
-            }
-        } else if (sign == '%') {
-            result = a % b;
-        }
         if (a < 0 || b < 0) {
             System.out.println("Введите целые положительные числа!!!");
         } else {
+            if (sign == '+') {
+                result = a + b;
+            } else if (sign == '-') {
+                result = a - b;
+            } else if (sign == '*') {
+                result = a * b;
+            } else if (sign == '/') {
+                result = a / b;
+            } else if (sign == '^') {
+                result = 1;
+                for (int i = 1; i <= b; i++) {
+                    result *= a;
+                }
+            } else if (sign == '%') {
+                result = a % b;
+            }
             System.out.println(a + " " + sign + " " + b + " = " + result);
         }
     }
