ccc59232f44b81024ee36e64a850f3f391bed096
Maksimelgsk <konovalo_ms@mail.ru>
2022-11-21 21:54:36 +0300

    Calculator_part2_21-11-2022

--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -3,16 +3,13 @@ package com.startjava.lesson_2_3_4.calculator;
 import java.util.Scanner;
 
 public class CalculatorTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RuntimeException {
         Scanner scan = new Scanner(System.in);
         String reply = "yes";
         while ("yes".equals(reply)) {
             System.out.print("Введите математическое выражение: ");
-            try {
-                calculate(scan.nextLine());
-            } catch (Exception error) {
-                System.err.println("Ошибка! Неккоректный ввод значений!!!");
-            }
+            int result = calculate(scan.nextLine());
+            System.out.print("Результат: " + result);
             do {
                 System.out.println("\nХотите продолжить вычисления? [yes/no]:");
                 reply = scan.nextLine();
@@ -20,31 +17,40 @@ public class CalculatorTest {
         }
     }
 
-    public static void calculate(String mathExpression) {
-        String[] partsExpression = mathExpression.split(" ");
-        int num1 = Integer.parseInt(partsExpression[0]);
-        int num2 = Integer.parseInt(partsExpression[2]);
-        char sign = partsExpression[1].charAt(0);
+    public static int calculate(String mathExpression) {
         try {
-            if (num1 < 0 || num2 < 0) {
-                throw new IllegalArgumentException();
+            String[] partsExpression = mathExpression.split(" ");
+            double num1 = Double.parseDouble(partsExpression[0]);
+            double num2 = Double.parseDouble(partsExpression[2]);
+            char sign = partsExpression[1].charAt(0);
+            if (num1 < 0 || num2 < 0 ) {
+                System.err.println("Ошибка! Введены отрицательные числа!");
+                return 0;
             }
-            try {
-                int result = switch (sign) {
-                    case '+' -> num1 + num2;
-                    case '-' -> num1 - num2;
-                    case '*' -> num1 * num2;
-                    case '/' -> Math.floorDiv(num1, num2);
-                    case '^' -> (int) Math.pow(num1, num2);
-                    case '%' -> num1 % num2;
-                    default -> throw new IllegalStateException();
-                };
-                System.out.print("Результат: " + result);
-            } catch (ArithmeticException error) {
-                System.err.println("Ошибка! Деление на ноль!");
+            if (num1 % 1 != 0 || num2 % 1 != 0) {
+                System.err.println("Ошибка! Введены не целые числа!");
+                return 0;
             }
-        } catch (IllegalArgumentException error) {
-            System.err.println("Ошибка! Введены отрицательные числа!!!");
+            if (num2 == 0) {
+                throw new ArithmeticException();
+            }
+            double result = switch (sign) {
+                case '+' -> num1 + num2;
+                case '-' -> num1 - num2;
+                case '*' -> num1 * num2;
+                case '/' -> num1 / num2;
+                case '^' -> Math.pow(num1, num2);
+                case '%' -> num1 % num2;
+                default -> throw new IllegalStateException();
+            };
+            return (int) result;
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException error) {
+            System.err.println("Ошибка! Неккоректный ввод значений!");
+        } catch (ArithmeticException error) {
+            System.err.println("Ошибка! Деление на ноль!");
+        } catch (IllegalStateException error) {
+            System.err.println("Ошибка! Неккоректный ввод знака!");
         }
+        return 0;
     }
 }
