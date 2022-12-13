29f41eecf29c408f20781a2606be332d3683c445
Maksimelgsk <konovalo_ms@mail.ru>
2022-12-13 20:19:19 +0300

    HW5_13-12-2022

---
+++ src/com/startjava/lesson_5/create_db.sql
@@ -0,0 +1,14 @@
+DROP DATABASE jaegers;
+CREATE DATABASE jaegers;
+\c jaegers
+CREATE TABLE jaegers (
+    id SERIAL PRIMARY KEY,
+    modelName TEXT,
+    mark      CHAR(6),
+    height    REAL,
+    weight    REAL,
+    status    TEXT,
+    origin    TEXT,
+    launch    DATE,
+    kaijuKill INTEGER
+ );
